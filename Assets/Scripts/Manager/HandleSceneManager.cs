bd2accda4dc12703a844ee2fde8efac3fc2cf8e7
jnguy405 <jnguy405@ucsc.edu>
2026-04-10 12:11:23 -0700

    Scene templates

--- Assets/Scripts/Manager/HandleSceneManager.cs
+++ Assets/Scripts/Manager/HandleSceneManager.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
 
+// Multi-scene manager: 
+// Main Menu, Credits, Guide, Pause Menu, and Game Over
+// Potion Room
+// minigames include Interview, Cooking, and Car MiniGame
 public class HandleSceneManager : MonoBehaviour
 {
     public static HandleSceneManager instance;
